1e9977c0edbb2928f470ddd7540546210db00cc9
miguellpastell <augustonovaes2007@gmail.com>
2026-08-13 16:15:01 -0300

    Add files via upload

---
+++ calculadora de media.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+
+{
+	float nota1, nota2, nota3, media;
+	printf("Digite a 1° nota  ");
+	scanf("%f", &nota1);
+
+	printf("Digite a 2° nota  ");
+	scanf("%f", &nota2);
+
+	printf("Digite a 3° nota  ");
+	scanf("%f", &nota3);
+
+	media = (nota1 + nota2 + nota3)/ 2.0;
+
+	printf("A média final é: %.2f\n", media);
+
+	return 0;
+}
